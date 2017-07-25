e025159a9a7405143286b443476d56792b54b48f
thomasrs <rb311515@dal.ca>
2017-07-24 21:55:20 -0300

    added test generate to make 10 files of random /tests files

---
+++ testGenerate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int testFiles = 10;//number of files in the folder created
+int sizeFiles = 5;//number of random packets in each file
+
+int main(){
+	int n = 1, i = 1;
+	for(n = 1; n <= testFiles; n++){
+
+		char name[12];
+
+		sprintf(name, "test%d.in", n);
+
+		FILE * fileN;
+		fileN = fopen(name, "w");
+
+		fprintf(fileN, "38080\n\n");
+		for(i = 1; i<=sizeFiles ; i++){
+		fprintf(fileN, "0.0 0.5 tests/%dkb.txt\n", rand()%128+1);
+		}
+	}
+
+return 0;
+}
